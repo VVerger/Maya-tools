ae89e3f2217b605f2184356301301087bf5f27ac
VVerger <158067222+VVerger@users.noreply.github.com>
2025-09-02 03:08:18 +0200

    Delete asset_manager directory

--- asset_manager/bin/__init__.py
+++
@@ -1,6 +0,0 @@
-from main import * 
-from ui import *
-from add import *
-from fileManagment import *
-from pages import *
-from refreshAll import * 
--- asset_manager/bin_Houdini/data.py
+++
@@ -1,3 +0,0 @@
-Main_Path = r'\\Storage03\Partages\3D4\KIVU'
-Ressource_Path = r'\\Storage03\Partages\3D4\KIVU\01_ressource\asset_manager\ressource'
-
